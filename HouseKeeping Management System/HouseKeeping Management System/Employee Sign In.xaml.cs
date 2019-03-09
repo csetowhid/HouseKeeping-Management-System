7fb931e226864a23597477180c9327f8f557f362
tanvirsabit <tanvirsabitnwusp15@gmail.com>
2019-03-09 23:21:36 +0600

    Employee Sign in page design

--- HouseKeeping Management System/HouseKeeping Management System/Employee Sign In.xaml.cs
+++ HouseKeeping Management System/HouseKeeping Management System/Employee Sign In.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace HouseKeeping_Management_System
 {
@@ -22,6 +24,45 @@ namespace HouseKeeping_Management_System
         public Employee_Sign_In()
         {
             InitializeComponent();
+        }
+
+        private void grid(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btn_sub(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-1A5KDLM;Initial Catalog=fall16;Integrated Security=True");
+            try
+            {
+                if (sqlcon.State == ConnectionState.Closed)
+                    sqlcon.Open();
+                string commandstring = "select COUNT(1) from Employee WHERE username=@username AND password=@password";
+                SqlCommand sqlcmd = new SqlCommand(commandstring, sqlcon);
+                sqlcmd.CommandType = CommandType.Text;
+                sqlcmd.Parameters.AddWithValue("@username", txtusername.Text);
+                sqlcmd.Parameters.AddWithValue("@password", pbpass.Password);
+                int count = Convert.ToInt32(sqlcmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    Employer_Dashboard ad = new Employer_Dashboard();
+                    ad.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Not matched");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
         }
     }
 }
